4b41f325f5749ec47133e89bbada95a640a07ac7
rayaherrera <rayaherrera@aol.com>
2019-09-23 09:01:23 -0500

    I was able to only get my sun drawn because i ran into some difficulties trying to name different turtles.

---
+++ Landscape.py
@@ -0,0 +1,18 @@
+import turtle
+
+turtle.bgcolor("light sky blue")
+turtle.color("yellow")
+turtle.penup()
+turtle.left(180)
+turtle.forward(235)
+turtle.right(90)
+turtle.forward(275)
+turtle.pendown()
+turtle.begin_fill()
+turtle.circle(70)
+turtle.end_fill()
+
+
+
+
+turtle.exitonclick()
